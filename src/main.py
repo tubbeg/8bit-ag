b5ccf307829b5a2a929791908e78663926d2e850
Tobias Gärdebro <tubbeg@gmail.com>
2025-02-04 23:47:44 +0100

    Game is now loading image created with Pyxel editor.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,36 @@
-import pyxel
-
-
-pyxel.init(160, 120)
+import pyxel # type: ignore
 
 
 def quit_game ():
     return pyxel.btnp(pyxel.KEY_ESCAPE) or pyxel.btnp(pyxel.KEY_Q)
 
-def update():
-    if quit_game():
-        pyxel.quit()
 
-def draw():
-    pyxel.cls(254)
-    pyxel.rect(10, 10, 20, 30, 0)
+# blt(x, y, img, u, v, w, h, [colkey], [rotate], [scale])
+# x y is the position in your game
+# img is which "page" in your image bank
+# u v is the position in the bank (i think)
+# colkey: is the color which will be transparent, for instance:
+#   if your image background (in the image bank) is black then
+#   setting colkey to 0 will make all black colors transparent (actually quite useful)
+# rotation in degrees
+# scale up to 1.0 (100 %)
 
-pyxel.run(update, draw)
+class App:
+    def __init__(self):
+        pyxel.init(160, 120)
+        pyxel.load("./res.pyxres")
+        self.x = 0
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
+        if quit_game():
+            pyxel.quit()
+
+    def draw(self):
+    
+        pyxel.cls(12)
+        pyxel.rect(10, 10, 20, 30, 25)
+        pyxel.blt(15,15,0, 0,0, 15,15, 0, 180)
+
+App()
